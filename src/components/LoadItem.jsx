97d366d80202cb9f64716086e3df1a5f82519e4d
Jamgoori <lks2354@naver.com>
2022-10-06 11:21:45 +0900

    styledcomponet 적용

--- src/components/LoadItem.jsx
+++ src/components/LoadItem.jsx
@@ -1,6 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import styled from "styled-components"
 
+const Flex = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    margin: 80px 320px 0;
+    row-gap: 78px ;
+`
+const Image = styled.img`
+   width: 380px;
+height: 380px;   
+`
+const StoreName = styled.div`
+    font-weight: 400;
+    color: #767676;
+    font-size: 16px;
+    line-height: 22px;
+    margin-top: 16px;
+`
+const ItemName = styled.div`
+    font-weight: 400;
+    color: #000000;
+    font-size: 18px;
+    line-height: 22px;
+    margin-top: 10px;
+`
+const Price = styled.div`
+    font-weight: 700;
+    color: #000000;
+    font-size: 24px;
+    line-height: 30px;
+    margin-top: 10px;
+`
 const LoadItem = () => {
     const [item,setItem] = useState([]);
 
@@ -13,15 +45,15 @@ const LoadItem = () => {
     })
 
   return (
-    <>
+    <Flex>
     {item.map(item=>{
         return(<div key={item.id}>
-            <img src={item.image}/>
-            <div >{item.store_name}</div>
-            <div>{item.product_name}</div>
-            {item.price} 원</div>)
+            <Image src={item.image}/>
+            <StoreName>{item.store_name}</StoreName>
+            <ItemName>{item.product_name}</ItemName>
+            <Price>{item.price} 원</Price></div>)
     })}
-    </>
+    </Flex>
   )
 }
 
