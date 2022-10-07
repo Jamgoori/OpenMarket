54891fd224b1bc6b6cd82be6325ff393372a7113
Jamgoori <lks2354@naver.com>
2022-10-07 15:43:08 +0900

    헤드 styledcomponent

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,14 +1,59 @@
 import React, { useEffect, useState } from 'react'
+import styled from "styled-components"
+const Header = styled.header`
+display: flex;
+    margin: 26px 320px;
+    align-items: center;
+`
+const Label = styled.label`
+  position: relative;
+  input {
+    border: 2px solid #21BF48;
+  border-radius: 50px;
+  width: 400px;
+  height: 46px;
+  margin-left: 30px;
+  padding: 0 22px;
+  }
+  button {
+    position: absolute;
+    top: 9px;
+    right: 22px;
+    border: none;
+    background-color: white;
+    width: 28px;
+    height: 28px;
+  }
+  button:hover{
+    cursor: pointer;
+  }
+  img{
+    width: 28px;
+    height: 28px;
+  }
+`
 
+const Nav = styled.nav`
+margin-left: auto;
+display: flex;
+align-items: center;
+`
+const Icon = styled.img`
+  width: 46px;
+  height: 50px;
+`
 const Head = () => {
   return (
     <>
-    <header>
+    <Header>
     <img src='../images/Logo-hodu.png'/>
-    <input type="text"></input>
-    <img src='../images/icon-shopping-cart.svg'></img>
-    <img src='../images/icon-user.svg'></img>
-    </header>
+    <Label>
+    <input type="text" placeholder='상품을 검색해보세요!'></input><button><img src='../images/search.png'/></button></Label>
+    <Nav>
+    <Icon src='../images/icon-shopping-cart.svg'></Icon>
+    <Icon src='../images/icon-user.svg'></Icon>
+    </Nav>
+    </Header>
     
     <main>
 
